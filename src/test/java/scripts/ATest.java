1aa239603970c0efc8a6963637d30cb4d8528e88
Devendralg <devendralg18@gmail.com>
2024-03-12 10:44:43 +0530

    hosa project guru

---
+++ src/test/java/scripts/ATest.java
@@ -0,0 +1,18 @@
+package scripts;
+
+import org.testng.annotations.Test;
+
+public class ATest {
+	
+	@Test
+	public void a()
+	{
+		System.out.println("1");
+	}
+	@Test
+	public void a1()
+	{
+		System.out.println("11");
+	}
+
+}
---
+++ src/test/java/scripts/aT2.java
@@ -0,0 +1,18 @@
+package scripts;
+
+import org.testng.annotations.Test;
+
+public class aT2 {
+	
+	@Test
+	public void b()
+	{
+		System.out.println("2");
+	}
+	@Test
+	public void a1()
+	{
+		System.out.println("21");
+	}
+
+}
